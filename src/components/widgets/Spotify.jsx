5c4ff54969c090bd97a6d4137a18ec84f7e9ba5e
Jonathan T. Risoldi <jonathantrisoldi@gmail.com>
2026-06-03 20:10:27 -0400

    Fix spotify oauth

--- src/components/widgets/Spotify.jsx
+++ src/components/widgets/Spotify.jsx
@@ -26,18 +26,61 @@ export default function Spotify() {
   }, [])
 
   useEffect(() => {
-    if (token) {
-      fetchCurrentTrack()
-      const interval = setInterval(fetchCurrentTrack, 5000)
-      return () => clearInterval(interval)
+    const params = new URLSearchParams(window.location.search)
+    const code = params.get('code')
+    if (code) {
+      exchangeToken(code)
+      window.history.replaceState({}, document.title, window.location.pathname)
     }
-  }, [token])
+  }, [])
 
-  const login = () => {
+  const exchangeToken = async (code) => {
     const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID
-    const redirectUri = encodeURIComponent(window.location.origin)
-    const url = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${redirectUri}&scope=${encodeURIComponent(SCOPES)}`
+    const redirectUri = window.location.origin
+    const codeVerifier = localStorage.getItem('spotify_code_verifier')
+
+    const res = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams({
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri: redirectUri,
+        client_id: clientId,
+        code_verifier: codeVerifier
+      })
+    })
+    const data = await res.json()
+    if (data.access_token) {
+      localStorage.setItem('spotify_token', data.access_token)
+      setToken(data.access_token)
+    }
+  }
+
+  const login = async () => {
+    const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID
+    const redirectUri = window.location.origin
+    
+    const codeVerifier = generateCodeVerifier()
+    const codeChallenge = await generateCodeChallenge(codeVerifier)
+    localStorage.setItem('spotify_code_verifier', codeVerifier)
+
+    const url = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(SCOPES)}&code_challenge_method=S256&code_challenge=${codeChallenge}`
     window.location.href = url
+  }
+
+  const generateCodeVerifier = () => {
+    const array = new Uint32Array(56)
+    crypto.getRandomValues(array)
+    return Array.from(array, dec => ('0' + dec.toString(16)).slice(-2)).join('')
+  }
+
+  const generateCodeChallenge = async (codeVerifier) => {
+    const encoder = new TextEncoder()
+    const data = encoder.encode(codeVerifier)
+    const digest = await crypto.subtle.digest('SHA-256', data)
+    return btoa(String.fromCharCode(...new Uint8Array(digest)))
+      .replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '')
   }
 
   const fetchCurrentTrack = async () => {
