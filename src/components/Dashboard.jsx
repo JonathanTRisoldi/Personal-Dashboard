7e917dde8c7ee7dc00212eff4b8d58701a86b91a
Jonathan T. Risoldi <jonathantrisoldi@gmail.com>
2026-06-04 23:44:46 -0400

    Added movable widgets on mobile

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,4 +1,4 @@
-import react from 'react'
+import React from 'react'
 import { useState, useEffect } from 'react'
 import { supabase } from '../supabase'
 import DateTimeWeather from './widgets/DateTimeWeather'
