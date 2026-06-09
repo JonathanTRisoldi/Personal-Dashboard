a723b644d45be4c89e7a76ea6645e5a31bc53e9f
Jonathan T. Risoldi <jonathantrisoldi@gmail.com>
2026-06-08 22:51:18 -0400

    add PWA support

---
+++ generate-icons.js
@@ -0,0 +1,9 @@
+import sharp from 'sharp'
+import { readFileSync } from 'fs'
+
+const svg = readFileSync('./public/favicon.svg')
+
+await sharp(svg).resize(192, 192).png().toFile('./public/pwa-192x192.png')
+await sharp(svg).resize(512, 512).png().toFile('./public/pwa-512x512.png')
+
+console.log('Icons generated!')
